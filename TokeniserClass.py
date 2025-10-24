8b2a1bbd46150feddd5aba5a842a5d5dfc220ba8
Beast-Bourne <137620684+Beast-Bourne@users.noreply.github.com>
2025-10-24 21:15:08 +0100

    moved vocab array generation into tokeniser class

--- TokeniserClass.py
+++ TokeniserClass.py
@@ -1,7 +1,24 @@
 import re
         
 class Tokeniser:
-    def __init__(self, vocab):
+    def __init__(self, dataset):
+        
+        # generates the vocab array from the dataset
+        # reads all customer given inputs then splits them into individual words and punctuation
+        vocab = []
+        for i in range(len(dataset)):
+            text = dataset['instruction'][i]
+            spliter = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+            spliter = [item for item in spliter if item.strip()]
+            vocab += spliter
+
+        # sorts the vocab array alphabetically then removes all duplicate entries
+        vocab = sorted(set(vocab))
+        vocab = {token:integer for integer, token in enumerate(vocab)}
+        
+        # pass a string in to Tokenise[] to get a token (a unique integer identifier)
+        # pass an integer into Detokenise[] to get its corrosponding string
+        # This only works for words and tokens in the 'vocab' dataset
         self.Tokenise = vocab
         self.Detokenise = {i:s for s,i in vocab.items()}
         
